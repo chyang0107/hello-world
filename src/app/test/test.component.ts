0ab6d92ccdb5a8b1371ccf0f75438aeffaf7328e
chyang0107 <chyang0107@gmail.com>
2021-03-25 23:19:24 -0500

    angular tutorial 5

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,16 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-          <div>
-            Hello World
-            </div>`,
-  styles: [`
-    div {
-      color: red;
-    }
-  `]
+          <h2>Welcome {{name}}</h2>
+          <h2>{{2+2}}</h2>
+          <h2>{{"Welcome "+ name}}</h2>
+          <h2>{{name.length}}</h2>
+          <h2>{{name.toUpperCase()}}</h2>
+          <h2>{{greetUser()}}</h2>
+          <!-- <h2>{{a = 2+2 }}</h2> -->
+          <h2>{{siteUrl}}</h2>
+
+
+          `,
+  styles: []
 })
 export class TestComponent implements OnInit {
+
+  public name = 'Hank';
+  public siteUrl = window.location.href;
+  greetUser() {
+    return "Hello " + this.name
+  }
 
   constructor() { }
 
