b34232ec52800c0add72dbd7b7cd4c6172f6d043
chyang0107 <chyang0107@gmail.com>
2021-04-18 11:18:52 -0500

    4/17 work

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,18 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-          <h2>Welcome {{name}}</h2>
-          <input #myInput type="text">
-          <button (click)="logMessage(myInput.value)" >Log</button>
+      <input [(ngModel)]= "name" type="text">
+      {{name}}
           `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public name = 'Hank';
-  logMessage(value) {
-    console.log(value)
-  }
+  public name = "";
+
 
   constructor() { }
 
